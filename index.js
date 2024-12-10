4098955f72944fd0a55437c162efd67007ba523d
JasonN13 <148019428+JasonN13@users.noreply.github.com>
2024-12-09 22:56:18 -0600

    Examen Segundo Parcial

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const sequelize =require('./config/database')
 const Empleados = require('./modelos/Empleados');
+const Productos = require('./modelos/Productos');
+const { Sequelize } = require('sequelize');
+const cors = require('cors');
 
 
 const  app = express();
@@ -9,13 +12,24 @@ app.use(express.json())
 
 var port = 5000;
 
+const corsRutas = {
+    origin:'http://localhost:3000'
+
+}
+
+app.use('/Valor_Promedio',cors(corsRutas));
+app.use('/Cantidad_Marca',cors(corsRutas));
+app.use('/Valor_TOTAL',cors(corsRutas));
+
+
+/*
 app.get('/suma-salario-departamento',async(req,resp)=>{
 
     try {
         const result = await Empleados.findAll({
             attributes:[
                 'DEPARTMENT_ID',
-                [sequelize.fn('SUM',sequelize.col('SALARY')), 'Salario Neto']
+                [sequelize.fn('SUM',sequelize.col('SALARY')), 'Salario_Neto']
             ],
             group:['DEPARTMENT_ID']
         })
@@ -34,7 +48,7 @@ app.get('/Nombre-Departamento-Salario',async(req,resp)=>{
             attributes:[
                 'DEPARTMENT_ID',
                 'JOB_ID',
-                [sequelize.fn('SUM',sequelize.col('SALARY','JOB_ID')), 'Salario Neto']
+                [sequelize.fn('SUM',sequelize.col('SALARY','JOB_ID')), 'Salario_Neto']
             ],
             group:['DEPARTMENT_ID','JOB_ID']
         })
@@ -53,7 +67,7 @@ app.get('/Empleados-Departamento',async(req,resp)=>{
         const result = await Empleados.findAll({
             attributes:[
                 'DEPARTMENT_ID',
-                [sequelize.fn('COUNT',sequelize.col('DEPARTMENT_ID')), 'Cantidad de Empleados']
+                [sequelize.fn('COUNT',sequelize.col('DEPARTMENT_ID')), 'Cantidad_de_Empleados']
             ],
             group:['DEPARTMENT_ID']
         })
@@ -64,8 +78,69 @@ app.get('/Empleados-Departamento',async(req,resp)=>{
         resp.status(500).json({error:'Ocurrio un error' + error})
     }
 })
+*/
 
+//Examen del segundo parcial 
+
+
+
+//SELECT categoryCode, AVG(CAST(value AS DECIMAL)) AS average_valueFROM productoGROUP BY categoryCode;
+app.get('/Valor_Promedio',async(req,resp)=>{
+
+    try {
+        const result = await Productos.findAll({
+            attributes:[
+                'categoryCode',
+                [sequelize.fn('AVG',sequelize.cast(sequelize.col('value'),'DECIMAL')),'average_value']
+            ],
+            group:['categoryCode'],
+        })
+        resp.json(result);
+    } catch (error) {
+        console.log('Error con el valor promedio de productos',error)
+        resp.status(500).json({error:'Ocurrio un error' + error})
+        
+    }
+})
+
+app.get('/Cantidad_Marca',async(req,resp)=>{
+
+    try {
+        const result = await Productos.findAll({
+            attributes:[
+                'brandCode',
+                [sequelize.fn('COUNT',sequelize.col('brandCode')),'Cantidad_Marcar']
+            ],
+            group:['brandCode'],
+        })
+        resp.json(result);
+    } catch (error) {
+        console.log('Error con la cantidad de procucto por marca',error)
+        resp.status(500).json({error:'Ocurrio un error' + error})
+        
+    }
+}) 
+
+app.get('/Valor_TOTAL',async(req,resp)=>{
+
+    try {
+        const result = await Productos.findAll({
+            attributes:[
+                'categoryCode',
+                [Sequelize.fn('SUM', Sequelize.cast(Sequelize.fn('REPLACE', Sequelize.col('value'), '$', ''), 'DECIMAL')), 'total_value']
+            ],
+            group:['categoryCode'],
+        })
+        resp.json(result);
+    } catch (error) {
+        console.log('Error con la cantidad de procucto por marca',error)
+        resp.status(500).json({error:'Ocurrio un error' + error})
+        
+    }
+}) 
 
 app.listen(port,()=>{
     console.log ('Aplicacion ejecutando en el puerto: ',port)
 })
+
+
